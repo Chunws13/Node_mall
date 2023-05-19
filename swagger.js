4c691f9267ac9db38b6f44622cd375904d6d8a88
Chunws13 <dntkd51@naver.com>
2023-05-19 21:03:08 +0900

    add swagger

---
+++ swagger.js
@@ -0,0 +1,21 @@
+const swaggerJsdoc = require('swagger-jsdoc');
+
+const swaggerOptions = {
+    swaggerDefinition: {
+        openapi: '3.0.0', // Swagger 버전
+        info: {
+            title: 'Node Mall API 문서', // API 문서 제목
+            version: '1.0.0', // API 버전
+            description: 'API 문서를 위한 Swagger',
+        },
+        servers: [{
+            url: 'http://localhost:3000', // 서버 URL
+        }, ],
+    },
+    apis: ['./routes/*.js'], // API 경로 설정
+
+};
+
+const swaggerDocs = swaggerJsdoc(swaggerOptions);
+
+module.exports = swaggerDocs;
