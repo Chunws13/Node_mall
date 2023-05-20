f6107d4b4851761078862d6f66692889dbd736eb
Chunws13 <dntkd51@naver.com>
2023-05-20 10:24:04 +0900

    add cors options

--- index.js
+++ index.js
@@ -3,17 +3,23 @@ const usersRouter = require('./routes/users.js');
 const sellersRouter = require('./routes/sellers.js');
 const swaggerUi = require('swagger-ui-express'); // swagger
 const swaggerDocs = require('./swagger.js');
+const cors = require('cors');
 const app = express();
 const port = 3000;
-/**
- *  @swagger
- *  tag :
- *      name: api/join
- *      description : 회원가입
- */
+
+const corsOption = {
+    origin: 'https://hanghae-shop-git-main-zadragon.vercel.app/',
+    credentials: true
+}
+
 app.use(express.json());
+app.use(cors);
 app.use('/docs-api', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 app.use('/api', [usersRouter, sellersRouter]);
+
+app.get('/', async(req, res) => {
+    return res.send(`Use '/docs-api' Page`);
+})
 
 app.listen(port, () => {
     console.log(`Server listen at ${port}`)
